9155ca76b64e4154aa0d6f61f1cd31c5c7d007f4
Luka Warren <lukawarren@icloud.com>
2019-07-20 15:57:57 +0100

    asd

---
+++ fe/js/fade.js
@@ -0,0 +1,28 @@
+$('.nav-link-fade').click(function() {
+
+    //event.preventDefault()
+
+    newLocation = this.href
+
+    
+
+    $('.section').fadeOut(1000, function() { 
+
+        console.log(this.href)
+
+        if (newLocation.includes("about"))
+            window.location = "search.html#search"
+        else
+            window.location = "index.html"
+
+        //newPage()
+
+    })
+
+})
+
+function newPage() {
+    
+    window.location = newLocation
+
+}

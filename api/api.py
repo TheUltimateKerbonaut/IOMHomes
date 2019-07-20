336b59acd33c01269ac104b19f8f9402ff24fca5
Siddharth Shakthivel Muthu Pandian <siddharth@Siddharths-MacBook-Pro.local>
2019-07-20 17:09:55 +0100

    Minimise's JSON file

--- api/api.py
+++ api/api.py
@@ -1,11 +1,11 @@
 import flask
+import json
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-
+    return jsonify(data)
+    
 app.run()

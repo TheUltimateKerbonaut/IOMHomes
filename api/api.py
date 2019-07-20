4b742f9b81b928d6c064200d78839b53eff6cafe
Luka Warren <lukawarren@icloud.com>
2019-07-20 20:48:20 +0100

    Update api.py

--- api/api.py
+++ api/api.py
@@ -62,8 +62,8 @@ def home():
         if len(correctEntries) > 100:
             return "Error: too many results"
 
-            if (len(correctEntries) == 0):
-                    return "Error: 0 results"
+        if (len(correctEntries) == 0):
+            return "Error: 0 results"
 
     return jsonify(correctEntries)
 

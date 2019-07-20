dbb2f6c9dc433b51f508bb17656925de3605b5f5
Luka Warren <lukawarren@icloud.com>
2019-07-20 19:59:12 +0100

    Minified JSON

--- python/Minimise JSON/minimiseJSON.py
+++ python/Minimise JSON/minimiseJSON.py
@@ -3,6 +3,6 @@ import sys
 
 file_data = open("../../data.json", "r", 1).read() # store file info in variable
 json_data = json.loads(file_data) # store in json structure
-json_string = json.dumps(json_data, separators=(',', ":", "{", "}")) # Compact JSON structure
+json_string = json.dumps(json_data, separators=(',', ':')) # Compact JSON structure
     
 open("output.json", "w+", 1).write(json_string) # open and write json_string to file

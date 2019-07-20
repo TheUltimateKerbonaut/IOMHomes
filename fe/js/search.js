7123a533b92eb19d28ecca25aade52bbc3fe1439
Luka Warren <lukawarren@icloud.com>
2019-07-20 20:11:37 +0100

    Minor issues

---
+++ fe/js/search.js
@@ -0,0 +1,8 @@
+function search()
+{
+    // Get search value first, focus on rest later
+    // Submit API request - the URL is http://87.254.85.85:5000?search=SearchGoesHere
+    // Print resulting JSON to console
+    // Watch out for errors!
+    // Have fun :)
+}

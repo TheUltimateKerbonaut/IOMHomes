f3c0ca57bc51a73a5b6e05f9b821d17c35848914
Siddharth Shakthivel Muthu Pandian <siddharth@Siddharths-MacBook-Pro.local>
2019-07-20 17:20:07 +0100

    Update minimiseJSON.py

--- python/Minimise JSON/minimiseJSON.py
+++ python/Minimise JSON/minimiseJSON.py
@@ -1,10 +1,8 @@
 import json
 import sys
 
-def minify(data.json):
-    file_data = open(file_name, "r", 1).read() # store file info in variable
-    json_data = json.loads(file_data) # store in json structure
-    json_string = json.dumps(json_data, separators=(',', ":")) # Compact JSON structure
-    file_name = str(file_name).replace(".json", "") # remove .json from end of file_name string
-    new_file_name = "{0}_minify.json".format(file_name)
-open(new_file_name, "w+", 1).write(json_string) # open and write json_string to file
+file_data = open("../../data.json", "r", 1).read() # store file info in variable
+json_data = json.loads(file_data) # store in json structure
+json_string = json.dumps(json_data, separators=(',', ":", "{", "}")) # Compact JSON structure
+    
+open("output.json", "w+", 1).write(json_string) # open and write json_string to file

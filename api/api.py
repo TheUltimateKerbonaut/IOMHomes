052621dc2c7cf5f6b06293ef00020e254efb2406
Luka Warren <lukawarren@icloud.com>
2019-07-20 19:51:01 +0100

    Finished API

--- api/api.py
+++ api/api.py
@@ -6,41 +6,58 @@ import os
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-    
-data = {}
-data = json.load(open("../data.json", "r"))
 
-@app.route('/api/', methods=['GET'])
+data = {}
+data = json.load(open("data.json", "r"))
+
+
+@app.route('/', methods=['GET'])
 def home():
 
     # Get entries matching search
-    string = request.args.get('search')
-
-    if (string == None):
-        string = ""
+    search = request.args.get('search')
 
     entries = []
 
     for entry in data:
         for row in entry:
-            if (string in entry[row]):
+            if (search == None or search in entry[row]):
                 entries.append(entry)
-    
-    if (len(entries) == 0):
-        return ""
 
     # Check if entries match dropdown criteria
     town = request.args.get('town')
-    price_range = request.args.get('price')
-    year = request.args.get('year')
+    price_min = request.args.get('price_min')
+    price_max = request.args.get('price_max')
+    acquisition_year = request.args.get('acquisition_year')
+    registered_year = request.args.get('registered_year')
 
     correctEntries = []
 
     for i in range(0, len(entries)):
-        if entries[i]["town"] == town:
-            correctEntries.append(entries[i])
 
-    return jsonify(entries)
-    # return jsonify(data)
+        year_acquisition = entries[i]["acquisition_date"].split(
+            "/")[2]  # Eg if date is 1/2/2005
+        # Then 2nd element in array split by the '/' characters would by 2005
+        year_registered = entries[i]["acquisition_date"].split("/")[2]
+
+        if town != None and entries[i]["town"].lower() != town.lower():
+            continue  # Skip current iteration in for loop. Unlike break, continue allows for future loop iterations
+
+        if (entries[i]["market_value"] == "" or price_min != None and float(entries[i]["market_value"]) < float(price_min)):
+            continue
+
+        if (entries[i]["market_value"] == "" or price_max != None and float(entries[i]["market_value"]) > float(price_max)):
+            continue
+
+        if acquisition_year != None and year_acquisition != acquisition_year:
+            continue
+
+        if registered_year != None and year_registered != registered_year:
+            continue
+
+        correctEntries.append(entries[i])
+
+    return jsonify(correctEntries)
+
 
 app.run()
